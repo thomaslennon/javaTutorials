8c27d31c9b92adec1b232b20d6aacd21fe496769
Thomas Lennon <thomas.lennon@ericsson.com>
2015-07-08 15:58:24 +0100

    adding the MIT License

--- testfirst/src/test/ie/tomlennon/NumberCheckerTests.java
+++ testfirst/src/test/ie/tomlennon/NumberCheckerTests.java
@@ -32,12 +32,4 @@ public class NumberCheckerTests {
     public void ensureNumberIsIn5To9Range_Checking_above_upper_boundary_returnFalse(){
         assertFalse(checker.ensureNumberIsIn5To9Range(15));
     }
-
-
-//    A test of a number less than 5. 2. returns false
-//    A test of the left bound. 5. returns true
-//    A test of a number in the middle of the range. 7 returns true.
-//    A test of the right bound. 9. Returns true
-//    A test of a number greater than 9. 11. returns false
-
 }

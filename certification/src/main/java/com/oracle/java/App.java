3869e080a6f75c690cb151c2c70773ece3be6f2d
Thomas Lennon <thomas.lennon@ericsson.com>
2015-12-14 14:25:32 +0000

    experimenting with alphabet for fields

--- certification/src/main/java/com/oracle/java/App.java
+++ certification/src/main/java/com/oracle/java/App.java
@@ -4,7 +4,17 @@ package com.oracle.java;
  * Hello world!
  */
 public class App {
+
+        private static final int test  =0;
+
         public static void main(String[] args) {
                 System.out.println("Hello World!");
+
+
         }
+
+        public boolean isHappy(final int face) {
+                return face == test;
+        }
+
 }
